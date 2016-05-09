fab7739735d816321220a411bb1d5b8bb55f6a4d
audriusitstep <audriusitstep@gmail.com>
2016-05-09 20:43:08 +0300

    only changes a to b

--- coder.rb
+++ coder.rb
@@ -3,9 +3,27 @@ class Coder
   attr_accessor :text
 
   def initialize(text)
+    @text = text
+  end
 
+  def permutation_matrix
+    {
+      'a' => 'b',
+      'b' => 'c'
+    }
+  end
+
+  def get_coded_text
+    text.split('').each do |char|
+      text.gsub!('a', 'b')
+    end
+    text
   end
 
 end
+
+source_file = File.open(ARGV[0], 'r')
+coder = Coder.new(source_file.read.chomp)
+print coder.get_coded_text
 
 
